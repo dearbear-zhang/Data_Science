9050a4b2bd953fe8daa1327bcec318e5b9c5ffa6
dearbear-zhang <dearbear_zhang@icloud.com>
2018-05-03 22:15:32 +0800

    MNIST练习加入评估模型,并添加注释说明

--- MachineLearning/TFStudy/MNIST.py
+++ MachineLearning/TFStudy/MNIST.py
@@ -5,16 +5,24 @@ mnist = input_data.read_data_sets('data/MNIST', one_hot=True)
 x = tf.placeholder("float", [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
+# SoftMax 回归 Y = sofmax(XW + B)
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
 y_ = tf.placeholder("float", [None, 10])
+# 定义损失函数,使用交叉熵作为损失函数
 cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+# 使用梯度下降法 以0.01的学习速率 最小化交叉熵
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 sess = tf.Session();
 sess.run(init)
+# 随机1000次,每次抽取100个数据进行训练化参数(随机梯度下降法) W,b
 for i in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={x:batch_xs, y:batch_ys})
+    sess.run(train_step, feed_dict={x:batch_xs, y_:batch_ys})
 
+# 评估模型,测试集测试正确率
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+print(sess.run(accuracy, feed_dict={x:mnist.test.images, y_:mnist.test.labels}))
 
