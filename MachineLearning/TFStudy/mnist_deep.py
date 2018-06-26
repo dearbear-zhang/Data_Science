bfc2f27c282a536de4e8745e05dd1b46d5e48b3f
dearbear-zhang <dearbear_zhang@icloud.com>
2018-06-27 00:12:55 +0800

    // mnist_deep 加入统计输出

--- MachineLearning/TFStudy/mnist_deep.py
+++ MachineLearning/TFStudy/mnist_deep.py
@@ -159,12 +159,16 @@ def train():
             if i % 100 == 0:
                 train_accuracy = accuracy.eval(feed_dict={
                     x: batch[0], y_: batch[1], keep_prob: 1.0})
+                summary = sess.run(merged_sunmary_op,feed_dict={
+                    x: batch[0], y_: batch[1], keep_prob: 1.0})
+                test_writer.add_summary(summary, i)
                 print('step %d, training accuracy %g' % (i, train_accuracy))
             train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
         print('test accuracy %g' % accuracy.eval(feed_dict={
             x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
-
+    train_writer.close()
+    test_writer.close()
 
 def main():
     """
